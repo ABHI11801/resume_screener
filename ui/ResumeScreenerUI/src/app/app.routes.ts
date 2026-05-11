dc94db7ebed616e7e3ce6c55ecd02cbe85fb8dee
unknown <abhinavkb2004@gmail.com>
2026-05-11 11:29:52 +0530

    guards and interceptor added

--- ui/ResumeScreenerUI/src/app/app.routes.ts
+++ ui/ResumeScreenerUI/src/app/app.routes.ts
@@ -1,3 +1,31 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { LoginComponent } from './auth/login/login.component';
+
+import { HomeComponent } from './dashboard/home/home.component';
+
+import { authGuard } from './guards/auth.guard';
+
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full'
+  },
+
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+
+  {
+    path: 'dashboard',
+    component: HomeComponent,
+    canActivate: [authGuard]
+  },
+
+  {
+    path: '**',
+    redirectTo: 'login'
+  }
+];
--- ui/ResumeScreenerUI/src/app/auth/login/login.component.ts
+++ ui/ResumeScreenerUI/src/app/auth/login/login.component.ts
@@ -1,11 +1,62 @@
 import { Component } from '@angular/core';
+import {FormBuilder,ReactiveFormsModule,Validators} from '@angular/forms';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { AuthService } from '../../services/auth.service';
 
-@Component({
-  selector: 'app-login',
-  imports: [],
-  templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
-})
+@Component({selector: 'app-login',standalone: true,imports: [CommonModule,ReactiveFormsModule],templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']})
 export class LoginComponent {
+  isLoading = false;
+  errorMessage = '';
+  loginForm;
+  constructor(
+    private fb: FormBuilder,
+    private authService: AuthService,
+    private router: Router
+  )
+  {
+    this.loginForm = this.fb.group({
+      email: ['', [
+        Validators.required,
+        Validators.email
+      ]],
 
+      password: ['', [
+        Validators.required
+      ]]
+    });
+  }
+
+  onSubmit(): void
+  {
+    if (this.loginForm.invalid)
+    {
+      return;
+    }
+    this.isLoading = true;
+    this.errorMessage = '';
+    this.authService.login(
+      this.loginForm.value
+    )
+    .subscribe({
+      next: (response) =>
+      {
+        this.authService.saveToken(
+          response.token
+        );
+
+        this.router.navigate([
+          '/dashboard'
+        ]);
+      },
+      error: () =>
+      {
+        this.errorMessage =
+          'Invalid email or password';
+
+        this.isLoading = false;
+      }
+    });
+  }
 }
