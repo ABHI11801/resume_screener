43722edcb271042ebbb7f93b591b5d2a5151115a
unknown <abhinavkb2004@gmail.com>
2026-05-11 13:51:43 +0530

    login logic and dashboard layout done

--- ui/ResumeScreenerUI/src/app/dashboard/home/home.component.ts
+++ ui/ResumeScreenerUI/src/app/dashboard/home/home.component.ts
@@ -1,11 +1,40 @@
 import { Component } from '@angular/core';
+import { CommonModule }from '@angular/common';
+import { RouterOutlet, Router }from '@angular/router';
+import { MatToolbarModule} from '@angular/material/toolbar';
+import { MatSidenavModule} from '@angular/material/sidenav';
+import { MatListModule} from '@angular/material/list';
+import {  MatButtonModule} from '@angular/material/button';
+import {  MatIconModule} from '@angular/material/icon';
+import { AuthService }from '../../services/auth.service';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  standalone: true,
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatListModule,
+    MatButtonModule,
+    MatIconModule
+  ],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
 
+  constructor(private authService: AuthService,private router: Router)
+  {
+  }
+
+  logout(): void
+  {
+    this.authService.logout();
+
+    this.router.navigate([
+      '/login'
+    ]);
+  }
 }
